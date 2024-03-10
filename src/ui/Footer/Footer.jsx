22f023fc9c0b96838738d446b32a2001fa456704
thagoo <lohitgowda56@gmail.com>
2024-03-10 23:06:27 +0530

    fix footer padding

--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -21,78 +21,75 @@ import { FaTelegram, FaYoutube } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer>
-      <Container>
-        <Row className="py-4">
-          <Col lg={3} md={6} className="mb-4 mb-md-0">
-            <h5 className={styles.listHeading}>Hobbycue</h5>
-            <ul className={`${styles.listContainer} list-unstyled`}>
-              <li className="mb-1">About us</li>
-              <li className="mb-1">Our services</li>
-              <li className="mb-1">Work with Us</li>
-              <li className="mb-1">FAQ</li>
-              <li className="mb-1">Contact Us</li>
-            </ul>
-          </Col>
+    <Container className="px-5">
+      <Row className="py-4">
+        <Col lg={3} md={6} className="mb-4 mb-md-0">
+          <h5 className={styles.listHeading}>Hobbycue</h5>
+          <ul className={`${styles.listContainer} list-unstyled`}>
+            <li className="mb-1">About us</li>
+            <li className="mb-1">Our services</li>
+            <li className="mb-1">Work with Us</li>
+            <li className="mb-1">FAQ</li>
+            <li className="mb-1">Contact Us</li>
+          </ul>
+        </Col>
 
-          <Col lg={3} md={6} className="mb-4 mb-md-0">
-            <h5 className={styles.listHeading}>Column 2</h5>
-            <ul className={`${styles.listContainer} list-unstyled`}>
-              <li className="mb-1">About us</li>
-              <li className="mb-1">Our services</li>
-              <li className="mb-1">Work with Us</li>
-              <li className="mb-1">FAQ</li>
-              <li className="mb-1">Contact Us</li>
-            </ul>
-          </Col>
+        <Col lg={3} md={6} className="mb-4 mb-md-0">
+          <h5 className={styles.listHeading}>Column 2</h5>
+          <ul className={`${styles.listContainer} list-unstyled`}>
+            <li className="mb-1">About us</li>
+            <li className="mb-1">Our services</li>
+            <li className="mb-1">Work with Us</li>
+            <li className="mb-1">FAQ</li>
+            <li className="mb-1">Contact Us</li>
+          </ul>
+        </Col>
 
-          <Col lg={3} md={6} className="mb-4 mb-md-0">
-            <h5 className={styles.listHeading}>Column 3</h5>
-            <ul className={`${styles.listContainer} list-unstyled`}>
-              <li className="mb-1">About us</li>
-              <li className="mb-1">Our services</li>
-              <li className="mb-1">Work with Us</li>
-              <li className="mb-1">FAQ</li>
-              <li className="mb-1">Contact Us</li>
-            </ul>
-          </Col>
+        <Col lg={3} md={6} className="mb-4 mb-md-0">
+          <h5 className={styles.listHeading}>Column 3</h5>
+          <ul className={`${styles.listContainer} list-unstyled`}>
+            <li className="mb-1">About us</li>
+            <li className="mb-1">Our services</li>
+            <li className="mb-1">Work with Us</li>
+            <li className="mb-1">FAQ</li>
+            <li className="mb-1">Contact Us</li>
+          </ul>
+        </Col>
 
-          {/* Fourth Column - Social Media Links */}
-          <Col lg={3} md={6} className="mb-4 mb-md-0">
-            <h5 className={styles.listHeading}>Social Media</h5>
-            <div className="mt-2">
-              <Stack direction="horizontal" gap={4}>
-                <FaFacebookF />
+        <Col lg={3} md={6} className="mb-4 mb-md-0">
+          <h5 className={styles.listHeading}>Social Media</h5>
+          <div className="mt-2">
+            <Stack direction="horizontal" gap={4}>
+              <FaFacebookF />
 
-                <FaTwitter />
+              <FaTwitter />
 
-                <FaInstagram />
+              <FaInstagram />
 
-                <FaLinkedinIn />
-                <FaTelegram />
-                <FaYoutube />
-              </Stack>
-            </div>
-            {/* Invite Friends Input */}
-            <Form className="mt-4">
-              <InputGroup className="d-flex input-group">
-                <Form.Control
-                  type="text"
-                  placeholder="Email ID"
-                  aria-label="email ID"
-                  aria-describedby="btnGroupAddon"
-                />
-                <Button variant="custom">Invite</Button>
-              </InputGroup>
-            </Form>
-          </Col>
-        </Row>
-        <Container className="text-center bg-light p-4">
-          <p className={styles.copyright}>
-            Lohith. Copyrights 2024. All rights reserved
-          </p>
-        </Container>
+              <FaLinkedinIn />
+              <FaTelegram />
+              <FaYoutube />
+            </Stack>
+          </div>
+
+          <Form className="mt-4">
+            <InputGroup className="d-flex input-group">
+              <Form.Control
+                type="text"
+                placeholder="Email ID"
+                aria-label="email ID"
+                aria-describedby="btnGroupAddon"
+              />
+              <Button variant="custom">Invite</Button>
+            </InputGroup>
+          </Form>
+        </Col>
+      </Row>
+      <Container className="text-center bg-light p-4">
+        <p className={styles.copyright}>
+          Lohith. Copyrights 2024. All rights reserved
+        </p>
       </Container>
-    </footer>
+    </Container>
   );
 }
